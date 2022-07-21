614f3f90d1db4c1864728971b411f0347e03a79a
Vanish <kobrvan2@gmail.com>
2022-07-21 12:22:52 +0300

    Created interface

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import json
+from tkinter import *
 from connection import Connect
 
 
 def main():
-	pass
+	root=Tk()
+	root.title("P2P tester")
+	root.geometry('300x300')
+	root.mainloop()
 
 
 
-if __name__='__main__':
+if __name__=='__main__':
 	main()
